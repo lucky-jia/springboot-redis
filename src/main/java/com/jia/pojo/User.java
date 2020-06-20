604bdbecb375ad77f679215d1c5fece20fe381f2
贾垂讯 <luckyjcx@126.com>
2020-06-20 21:16:01 +0800

    Revert "no message"
    This reverts commit ce283a714e4e0d53647ac03a12c848bf80d350e7.

--- src/main/java/com/jia/pojo/User.java
+++ src/main/java/com/jia/pojo/User.java
@@ -1,5 +1,8 @@
 package com.jia.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
@@ -9,39 +12,10 @@ import java.io.Serializable;
  * @date 2020-05-05 16:42
  */
 @Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User implements Serializable {
     private String name;
     private Integer age;
-
-    public User() {
-    }
-
-    public User(String name, Integer age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
