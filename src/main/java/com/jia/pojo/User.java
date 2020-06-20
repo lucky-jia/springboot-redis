aebb24e1869051bbc470886df47b5c31117a1a9e
贾垂讯 <luckyjcx@126.com>
2020-06-20 21:18:42 +0800

    no message

--- src/main/java/com/jia/pojo/User.java
+++ src/main/java/com/jia/pojo/User.java
@@ -12,10 +12,39 @@ import java.io.Serializable;
  * @date 2020-05-05 16:42
  */
 @Component
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class User implements Serializable {
     private String name;
     private Integer age;
+
+    public User() {
+    }
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
